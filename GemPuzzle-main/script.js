8d0cee85b38bc7454ccfaa561aef5e7c61a143b0
lusia <panfluba@gmail.com>
2022-10-24 23:51:02 +0300

    init: make basic things

---
+++ GemPuzzle-main/script.js
@@ -0,0 +1,78 @@
+"use strict";
+var element_counter = document.getElementById('counter');
+var counter = 0;
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+var size = urlParams.get('size') || 4;
+if(size < 3) { size = 3; }
+if(size > 6) { size = 6 }
+var boardTiles = size * size;
+var tiles = [];
+
+
+function shuffleTiles() {
+    for(var i = 0; i < 1000; i++) {
+        
+        handleClick({ srcElement: {index: Math.floor(Math.random() * boardTiles)}})
+        counter = 0;
+        element_counter.textContent = counter;
+    }   
+}
+
+
+function createCell() {
+    var table = document.getElementById("table");
+
+    for(var i = 0; i < size; i++) {
+        var tr = document.createElement("tr");
+
+        for(var j = 0; j < size; j++) {
+            var td = document.createElement("td");
+            var index = i * size + j;
+            td.className = "tile";
+            td.index = index;
+            td.value = index;
+            td.textContent = index == 0 ? "" : index;
+            td.onclick = handleClick;
+            tr.appendChild(td);
+            tiles.push(td);
+        }
+        table.appendChild(tr);
+
+    }
+    shuffleTiles();
+}
+
+function swap(i, j) {
+    // var counter = 0;
+    var temp = tiles[i].value;
+    tiles[i].textContent = tiles[j].textContent;
+    tiles[i].value = tiles[j].value;
+    tiles[j].textContent = temp;
+    tiles[j].value = temp;
+    counter++;
+    element_counter.textContent = counter;
+}
+
+function handleClick(e) {
+    var boardTiles = size * size;
+    var i = e.srcElement.index;
+
+
+    if(i - size >= 0 && tiles[i - size].value == 0) {
+        swap(i, i - size);
+
+    } else if(i + size < size * size && tiles[i + size].value == 0) {
+        swap(i, i + size);
+    } else if(i % size != 0 && tiles[i - 1].value == 0) {
+        swap(i, i - 1);
+    } else if(i % size != size - 1 && tiles[i + 1].value == 0) {
+        swap(i, i + 1);
+    }
+}
+
+
+
+
+
+
---
+++ GemPuzzle-main/timer.js
@@ -0,0 +1,64 @@
+
+// var span_minute = document.getElementsById('minute');
+// var span_second  = document.getElementsById('second');
+// var start = document.getElementById('strt');
+var h3 = document.getElementsByTagName('p')[0];
+var stop_b = document.getElementById('stop_button');
+var restart = document.getElementById('restart_button');
+var sec = 0;
+var min = 0;
+var i = 0;
+var t;
+
+
+
+function tick() {
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = '##';
+            sec = '##'
+        }
+    }
+}
+
+function add() {
+    tick();
+    h3.textContent = (min > 9 ? min : "0" + min) + " : " + (sec > 9 ? sec : "0" + sec);
+    // span_minute.textContent = (min > 9 ? min : "0" + min);
+    // span_second.textContent =  (sec > 9 ? sec : "0" + sec);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
+timer();
+window.DOMContentLoaded = timer;
+stop_b.onclick = function () {
+    stop_b.style.backgroundColor = 'grey'
+    clearTimeout(t);
+    i++;
+    if (i == 2) {
+        i = 0;
+        stop_b.style.backgroundColor = '';
+        timer();
+    }
+
+    // this.addEventListener('click', function{
+    //     stop_b.style.backgroundColor = ''
+    //     timer();
+    // })
+}
+restart.addEventListener('click', function () {
+    stop_b.style.backgroundColor = ''
+    h3.textContent = "00 : 00";
+    seconds = 0; minutes = 0;
+    sec = 0;
+    min = 0;
+    i = 0;
+    clearTimeout(t);
+    timer();
+})
